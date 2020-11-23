4f941c8d5be47abd2d2d6b92bb7885af3a9ba565
Minakshi Mukherjee <adaboost.one@gmail.com>
2020-11-22 20:08:33 -0800

    Create node.h

---
+++ reverse/node.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+
+struct Node {
+    int data;
+    Node *next;
+};
+
+
+struct StringNode {
+    std::string data;
+    StringNode *next;
+};
+
+struct DoubleNode {
+    double data;
+    DoubleNode *next;
+};
